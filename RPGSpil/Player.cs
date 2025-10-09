a18bf70842c30aaecb7fd66d48053b9d143463bb
Acidity <121935839+Rasmus363h@users.noreply.github.com>
2025-10-09 21:23:22 +0200

    energi change

--- RPGSpil/Player.cs
+++ RPGSpil/Player.cs
@@ -2,13 +2,13 @@ namespace RPGSpil;
 
 public class Player
 {
-    private int _liv;
+    private int _liv = 20;
     public int Liv                        
     {                                     
         get { return this._liv; }         
         set { this._liv = value; }        
     }                                     
-    private int _energi;
+    private int _energi = 10;
     public int Energi
     {
         get { return this._energi; }
@@ -18,12 +18,13 @@ public class Player
     Random rnd = new Random();     
     (int damage, bool crit) light()
     {
-        int attack = rnd.Next(2, 3);
+        int attack = rnd.Next(1, 2);
         bool trueorfalse = crit();
         if (trueorfalse = true)
         {
             attack = (int)Math.Round(attack * 1.5);
         }
+        _energi -= 1;
         return (attack, trueorfalse);
     }
     (int damage, bool crit) medium()
@@ -34,16 +35,18 @@ public class Player
         {
             attack = (int)Math.Round(attack * 1.5);
         }
+        _energi -= 2;
         return (attack, trueorfalse);
     }
     (int damage, bool crit) heavy()
     {
-        int attack = rnd.Next(2, 3);
+        int attack = rnd.Next(3, 4);
         bool trueorfalse = crit();
         if (trueorfalse = true)
         {
             attack = (int)Math.Round(attack * 1.5);
         }
+        _energi -= 3;
         return (attack, trueorfalse);
     }
     public int block()
