97f671b87ce28ae3fda0ae82bf86059683910a01
Acidity <121935839+Rasmus363h@users.noreply.github.com>
2025-10-09 21:30:01 +0200

    changes

--- RPGSpil/Goblin.cs
+++ RPGSpil/Goblin.cs
@@ -2,5 +2,66 @@ namespace RPGSpil;
 
 public class Goblin
 {
+    private int _liv = 20;
+    public int Liv                        
+    {                                     
+        get { return this._liv; }         
+        set { this._liv = value; }        
+    }                                     
+    private int _energi = 10;
+    public int Energi
+    {
+        get { return this._energi; }
+        set { this._energi = value; }
+    }
     
+    Random rnd = new Random();     
+    (int damage, bool crit) light()
+    {
+        int attack = rnd.Next(1, 2);
+        bool trueorfalse = crit();
+        if (trueorfalse = true)
+        {
+            attack = (int)Math.Round(attack * 1.5);
+        }
+        _energi -= 1;
+        return (attack, trueorfalse);
+    }
+    (int damage, bool crit) medium()
+    {
+        int attack = rnd.Next(2, 3);
+        bool trueorfalse = crit();
+        if (trueorfalse = true)
+        {
+            attack = (int)Math.Round(attack * 1.5);
+        }
+        _energi -= 2;
+        return (attack, trueorfalse);
+    }
+    (int damage, bool crit) heavy()
+    {
+        int attack = rnd.Next(3, 4);
+        bool trueorfalse = crit();
+        if (trueorfalse = true)
+        {
+            attack = (int)Math.Round(attack * 1.5);
+        }
+        _energi -= 3;
+        return (attack, trueorfalse);
+    }
+    public int block()
+    {
+        int blocked = rnd.Next(1, 3);     
+        return blocked;
+    }
+
+    public bool crit()
+    {
+        int Crit = rnd.Next(1, 5);
+        if (Crit == 1)
+        {
+            return true;
+        }
+        return false;
+    }
 }
--- RPGSpil/Program.cs
+++ RPGSpil/Program.cs
@@ -5,8 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Player Player1 = new Player();
-        Random rnd = new Random();    
-        int attack = rnd.Next(4, 4);
-        Console.WriteLine(attack);
+        Goblin Goblin1 = new Goblin();
+        
     }
 }
